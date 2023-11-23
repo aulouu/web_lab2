4e5a3500060b3a3b8938d137972aed867d0e9484
DemonLord <tsostanov@gmail.com>
2023-11-23 17:55:54 +0300

    Initial commit

---
+++ src/main/java/beans/Point.java
@@ -0,0 +1,42 @@
+import java.io.Serializable;
+
+public class Point implements Serializable {
+    private double x;
+    private double y;
+    private double radius;
+    private boolean status;
+
+    public Point() {
+
+    }
+
+    public Point(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
